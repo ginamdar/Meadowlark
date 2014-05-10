0f986886b8477f25b1de4c9258fd96280e715708
Guruprasad Inamdar <guru.inamdar@gmail.com>
2014-05-10 02:07:56 -0400

    Move fortunecookie feature in seperate module.

--- meadowlark.js
+++ meadowlark.js
@@ -3,12 +3,8 @@ var http      = require("http");
 var app 	   = express();
 // set up handlebars view engine
 var handlebars = require("express3-handlebars").create({defaultLayout:"main"});
+var fortune = require("./lib/fortune.js");
 
-var fortuneCookies = ['conquer your fears or they will conquer you.',
-				'rivers need springs.',
-				'do not fear what you don\'t know.',
-				'you will have a pleasant surprise.',
-				'whenever possible, keep it simple.'];
 
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
@@ -25,8 +21,7 @@ app.get("/", function(req, resp){
 app.get("/about", function(req, resp){
 //	resp.type("text/plain");
 //	resp.send("About Meadowlark Travel");
-	var randomFuture = fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-	resp.render("about", {fortune: randomFuture});
+	resp.render("about", {fortune: fortune.getFortune()});
 });
 
 app.use(function(req, resp, next){
